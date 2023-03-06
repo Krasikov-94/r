8ba59522b2c6ffd1f85e891157bebdcd0e602f88
Anton <Krasikov-94@bk.ru>
2023-03-06 12:19:01 +0300

    add setC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer';
+import { Main } from './components/Main';
+import React from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  console.log('render');
   return (
     <div className="App">
       <Header />
 
-      {/* <Main /> */}
+      <hr />
+      {count}
+      <hr />
+
+      <button onClick={() => setCount(count - 1)}> Minus ONE </button>
+      <button onClick={() => setCount(count + 1)}> Plus ONE </button>
+      <Main />
 
       <Footer />
     </div>
